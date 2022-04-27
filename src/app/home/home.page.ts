8bdc2524498bfd0d70110c46779f2e2d099af809
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2022-04-27 07:44:29 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { environment } from './../../environments/environment';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
 
 @Component({
   selector: 'app-home',
@@ -7,6 +13,28 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  weatherTemp: any;
+  todayDate = new Date();
+  cityName: any = '';
+  name = '';
+  weatherIcon: any;
+  weatherDetails: any;
+  loading = true;
+
+  constructor(public httpClient: HttpClient) {
+    // this.loadData();
+  }
+
+  loadData() {
+    this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(result => {
+      console.log(result);
+      this.weatherTemp = result['main'];
+      this.name = result['name'];
+      this.weatherDetails = result['weather'][0];
+      console.log(this.weatherTemp);
+      this.weatherIcon = `http://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`;
+      this.loading = false;
+    });
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '0eaa7b16fc3f12257c0318e77faea19d',
+  API_URL: 'https://api.openweathermap.org/data/2.5/'
 };
 
 /*
